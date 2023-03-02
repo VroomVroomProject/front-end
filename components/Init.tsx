ed33593234889ce615ad4dcbc1d21237a2fdf995
TAEYANG <explore2012@naver.com>
2023-03-02 11:02:25 +0900

    feat: react-query 설정 및 테스트

--- components/Init.tsx
+++ components/Init.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 function Init() {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+      res.json(),
+    ),
+  );
+
+  if (error) {
+    return <div className="flex"> An error has occurred</div>;
+  }
+  if (isLoading) return <div className="flex">Loading...</div>;
   return (
     <div className="flex">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
