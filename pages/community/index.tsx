cc4f3ea2a6033845b527dd7afe8608cc9c123cc2
TAEYANG <explore2012@naver.com>
2023-03-04 23:59:31 +0900

    feat: 커뮤니티 기본 구성

---
+++ pages/community/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Community() {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h3>커뮤니티</h3>
+
+      <div className="">
+        <select name="" id="">
+          <option value="">전체</option>
+          <option value="">제목</option>
+          <option value="">내용</option>
+        </select>
+        <input type="text" />
+        <button>검색</button>
+      </div>
+    </div>
+  );
+}
+
+export default Community;
